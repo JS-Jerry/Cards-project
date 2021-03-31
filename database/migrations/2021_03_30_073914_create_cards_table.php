ccc918af23cfef04f26470ff2e433647f334d004
Jerry <jurek310@vp.pl>
2021-03-31 19:44:38 +0200

    save1

---
+++ database/migrations/2021_03_30_073914_create_cards_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCardsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cards', function (Blueprint $table) {
+            $table->id();
+            $table->decimal('card', 20, 0);
+            $table->decimal('pin', 4, 0);
+            $table->dateTime('activation', 0);
+            $table->date('expiration');
+            $table->bigInteger('balance')->nullable()->default(0)->comment('The value is given in pennies');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cards');
+    }
+}
